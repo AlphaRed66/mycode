1bbd6f0dd16989708b5ca1e344acd4d9c33f6110
AlphaRed66 <zombie5yd66@gmail.com>
2023-04-03 17:07:23 +0000

    learning about go basics

---
+++ goBasicksChallenge01.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+        var name, city, state  = "Jenn", "Pelzer", "SC"
+        fmt.Println("My name is ", name)
+        fmt.Println("I live in ", city + ", ", state)
+    }
